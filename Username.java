e2a9681e44383892b0023a962ad2a885f8cc4020
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2019-02-23 10:19:21 +0530

    Add files via upload

---
+++ Username.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Username{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the name");
+        String name=sc.nextLine();
+        System.out.println("Welcome "+name+".");
+    }
+}
