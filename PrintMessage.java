1c4814299681990ac90601cd1fdf5e0935184836
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2019-02-23 10:15:35 +0530

    Add files via upload

---
+++ PrintMessage.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class PrintMessage{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to object oriented programming");
+    }
+}
