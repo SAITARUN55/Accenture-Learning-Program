68fc7ca6d508fd632a929089dc7f65bd53a55df4
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2019-06-09 10:45:44 +0530

    Create Count Vowels.java

---
+++ MockTests/Count Vowels.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    
+    public static void main(String args[])
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U')
+            {
+                c++;
+            }
+        }
+        System.out.print(c);
+       
+    }
+}
