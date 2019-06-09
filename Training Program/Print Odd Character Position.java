d353b796ea709e65e0d4b7a58def2ca0b8217b11
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2019-06-09 11:08:54 +0530

    Create Print Odd Character Position.java

---
+++ Training Program/Print Odd Character Position.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        for(int i=0;i<s.length();i+=2)
+        {
+            System.out.print(s.charAt(i));
+        }
+}
+}
