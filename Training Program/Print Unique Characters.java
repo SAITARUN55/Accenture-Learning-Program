531e5523a159074be8095b135d90f0c4f3385119
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2019-06-10 09:47:50 +0530

    Create Print Unique Characters.java

---
+++ Training Program/Print Unique Characters.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        for(int i=0;i<s.length();i++)
+        {
+            int a=0;
+            for(int j=0;j<s.length();j++)
+            {
+                if(s.charAt(i)==s.charAt(j))
+                {
+                    a++;
+                }
+            }
+            if(a%2==1)
+            {
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+}
